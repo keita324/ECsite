ab2b30c407ed2e06922890f04be6f135dddd7371
EC2 Default User <ec2-user@ip-172-31-63-147.ec2.internal>
2022-11-23 15:14:39 +0000

    8-finish

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,16 @@
 class Public::ItemsController < ApplicationController
     def index
-        @items = Item.where(status: 'true')
+        @genres = Genre.all
+        if params[:genre_id]
+          # Categoryのデータベースのテーブルから一致するidを取得
+          @genre = Genre.find(params[:genre_id])
+          # category_idと紐づくを取得
+          @items = @genre.items.all
+          @title = @genre.name
+        else
+          @items = Item.where(status: 'true')
+          @title = "商品"
+        end
     end 
     
     def show
